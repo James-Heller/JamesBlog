b1bf7c3ed61e26423f9d1daf4c8ca99851fa5e73
JamesTang <James-Heller@Outlook.com>
2024-05-09 09:09:12 +0800

    Just init my blog

--- src/config.ts
+++ src/config.ts
@@ -7,9 +7,9 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Fuwari',
-  subtitle: 'Demo Site',
-  lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja'
+  title: 'JamesBlog',
+  subtitle: 'Personal blog powered by Fuwari',
+  lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: false,     // Hide the theme color picker for visitors
@@ -34,7 +34,7 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.About,
     {
       name: 'GitHub',
-      url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
+      url: 'https://github.com/James-Heller',     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
     },
   ],
@@ -42,15 +42,15 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  name: 'JamesTang',
+  bio: '我本将心向明月，奈何明月照沟渠',
   links: [
     {
-      name: 'Twitter',
-      icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
+      name: 'Gitee',
+      icon: 'simple-icons:gitee',       // Visit https://icones.js.org/ for icon codes
                                         // You will need to install the corresponding icon set if it's not already included
                                         // `pnpm add @iconify-json/<icon-set-name>`
-      url: 'https://twitter.com',
+      url: 'https://gitee.com/jamestangakajava9',            // Internal links should not include the base path, as it is automatically added
     },
     {
       name: 'Steam',
@@ -60,7 +60,7 @@ export const profileConfig: ProfileConfig = {
     {
       name: 'GitHub',
       icon: 'fa6-brands:github',
-      url: 'https://github.com/saicaca/fuwari',
+      url: 'https://github.com/James-Heller',
     },
   ],
 }
--- src/content/spec/about.md
+++ src/content/spec/about.md
@@ -1,9 +1,29 @@
 # About
-This is the demo site for [Fuwari](https://github.com/saicaca/fuwari).
 
-::github{repo="saicaca/fuwari"}
+Welcome to my personal blog! Here, I share my thoughts, experiences, and insights on various topics. Feel free to explore and engage with the content.
 
-> ### Sources of images used in this site
-> - [Unsplash](https://unsplash.com/)
-> - [星と少女](https://www.pixiv.net/artworks/108916539) by [Stella](https://www.pixiv.net/users/93273965)
-> - [Rabbit - v1.4 Showcase](https://civitai.com/posts/586908) by [Rabbit_YourMajesty](https://civitai.com/user/Rabbit_YourMajesty)
+### About Me
+
+I am James, a passionate writer and avid learner. I have a keen interest in technology, programming, and all things related to software development. Through this blog, I aim to share my knowledge and experiences with the community.
+
+### Topics Covered
+
+On this blog, you can expect to find articles and tutorials on a wide range of topics, including:
+
+- Programming languages and frameworks
+- Web development
+- Software engineering best practices
+- Tips and tricks for developers
+- Personal anecdotes and reflections
+
+### Contact Me
+
+If you have any questions, suggestions, or just want to say hello, feel free to reach out to me. You can contact me through the following channels:
+
+- Email: James-Heller@Outlook.com
+- Twitter: [@jamesblog](https://twitter.com/jamesblog)
+- LinkedIn: [James Blog](https://www.linkedin.com/in/jamesblog)
+
+Thank you for visiting my blog. I hope you find the content valuable and enjoyable!
+
+Happy reading!
