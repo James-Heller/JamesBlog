b65a198e97c5d2984aac9af522c9bb5e66d9ecaa
JamesTang <James-Heller@Outlook.com>
2024-12-09 17:42:59 +0800

    修改Banner和头像

--- src/config.ts
+++ src/config.ts
@@ -47,7 +47,7 @@ export const navBarConfig: NavBarConfig = {
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  avatar: 'assets/images/avatar.jpg',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: 'JamesTang',
   bio: '我本将心向明月，奈何明月照沟渠',
   links: [
